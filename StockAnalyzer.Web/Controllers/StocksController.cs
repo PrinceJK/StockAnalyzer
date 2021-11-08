d15ba6d3d696202fbef15432132b2e23cbcb2813
John Kehinde <kehindejohn018@gmail.com>
2021-11-08 15:26:08 +0100

    Stock Analyzer web and WPF app

---
+++ StockAnalyzer.Web/Controllers/StocksController.cs
@@ -0,0 +1,23 @@
+﻿using StockAnalyzer.Core;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace StockAnalyzer.Web.Controllers
+{
+    public class StocksController : ApiController
+    {
+        [Route("api/stocks/{ticker}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(string ticker)
+        {
+            var store = new DataStore(HostingEnvironment.MapPath("~/bin"));
+
+            var data = await store.LoadStocks();
+
+            if (!data.ContainsKey(ticker)) return NotFound();
+
+            return Json(data[ticker]);
+        }
+    }
+}
